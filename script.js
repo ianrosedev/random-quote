d0d837ebda7e1e7b925655a167a80c83c3edf218
Ian Rose <ianrosedev@gmail.com>
2016-03-22 03:54:12 -0400

    Final tweaks for now

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ $('#get-another-quote-button').on('click', function(e) {
     url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
     success: function(data) {
       var post = data.shift(); // The data is an array of posts. Grab the first one.
-      var quoteNoHTML = post.content.replace(/<\/?[^>]+>/gi, '');
+      var quoteNoHTML = post.content.replace(/<\/?[^>]+>/gi, ''); // Gets rid of HTML code from JSON.
       $('#quote-content').html(quoteNoHTML);
       $('#quote-title').html(post.title);
-      $("#twitter").attr("href", "https://twitter.com/home?status=" + quoteNoHTML + '- ' + post.title)
+      $("#twitter").attr("href", "https://twitter.com/home?status=" + quoteNoHTML + ' -' + post.title)
     },
     cache: false
   });
